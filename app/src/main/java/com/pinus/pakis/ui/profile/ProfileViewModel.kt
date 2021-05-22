9d00759294b0d1d93876e90336863fecdd0049b5
Deanira F <52442666+deanira@users.noreply.github.com>
2021-05-22 19:04:03 +0700

    change notifications into profile

--- app/src/main/java/com/pinus/pakis/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/pinus/pakis/ui/profile/ProfileViewModel.kt
@@ -1,10 +1,10 @@
-package com.pinus.pakis.ui.notifications
+package com.pinus.pakis.ui.profile
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class ProfileViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
