b6cfc3e238a74427d057f7194033d003b6dd49fc
Deanira F <52442666+deanira@users.noreply.github.com>
2021-05-25 21:01:12 +0700

    string

---
+++ app/src/main/java/com/pinus/pakis/ui/WebviewActivity.kt
@@ -0,0 +1,12 @@
+package com.pinus.pakis.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pinus.pakis.R
+
+class WebviewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_webview)
+    }
+}
