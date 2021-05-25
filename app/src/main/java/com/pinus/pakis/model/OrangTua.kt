e19c16091bfe206c1187fb149c2479cf973432d0
Deanira F <52442666+deanira@users.noreply.github.com>
2021-05-25 23:22:23 +0700

    firebase

---
+++ app/src/main/java/com/pinus/pakis/model/OrangTua.kt
@@ -0,0 +1,10 @@
+package com.pinus.pakis.model
+
+data class OrangTua(
+    val id: String,
+    val nama: String,
+    val email: String,
+    val password: String
+) {
+    constructor(): this("", "", "", "")
+}
--- app/src/main/java/com/pinus/pakis/ui/SignupActivity.kt
+++ app/src/main/java/com/pinus/pakis/ui/SignupActivity.kt
@@ -2,28 +2,72 @@ package com.pinus.pakis.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.database.*
 import com.pinus.pakis.databinding.ActivitySignupBinding
+import com.pinus.pakis.model.OrangTua
 
 class SignupActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignupBinding
+    private lateinit var ref: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.hide()
+
+        ref = FirebaseDatabase.getInstance().getReference("orangtua")
+
         binding.btnDaftar.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            signUp()
         }
 
         binding.tvMasuk.setOnClickListener {
             val intent = Intent(this, SigninActivity::class.java)
             startActivity(intent)
         }
+    }
 
-        supportActionBar?.hide()
+    private fun signUp() {
+        var isEmptyFields = false
+
+        with(binding) {
+            val nama = etName.text.toString().trim()
+            val email = etEmail.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+
+            when {
+                nama.isEmpty() -> {
+                    isEmptyFields = true
+                    etName.error = "Field ini tidak boleh kosong"
+                }
+                email.isEmpty() -> {
+                    isEmptyFields = true
+                    etEmail.error = "Field ini tidak boleh kosong"
+                }
+                password.isEmpty() -> {
+                    isEmptyFields = true
+                    etPassword.error = "Field ini tidak boleh kosong"
+                }
+            }
+
+            if (!isEmptyFields) {
+                Log.d("halo", "tes firebase")
+                val orangTuaId = ref.push().key
+                val orangTua = OrangTua(orangTuaId!!, nama, email, password)
+
+                ref.child(orangTuaId).setValue(orangTua).addOnCompleteListener {
+                    Snackbar.make(root, "Akun anda berhasil dibuat", Snackbar.LENGTH_SHORT).show()
+
+                    val intent = Intent(applicationContext, MainActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
     }
 }
